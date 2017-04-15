d2593c4db46c76644a27c4e05f22feb5864830b5
Matteo Suppo <matteo.suppo@gmail.com>
2017-04-15 19:19:23 +0200

    Make action editable

--- src/app/move/action/action.component.ts
+++ src/app/move/action/action.component.ts
@@ -1,14 +1,42 @@
 import { Component, Input } from '@angular/core';
 
 import { Action } from '../move';
+import { Animations } from '../animations';
 
 @Component({
   selector: 'ap-move-action',
   templateUrl: './action.component.html',
-  styleUrls: ['./action.component.css']
+  styleUrls: ['./action.component.css'],
+  animations: [
+    Animations.Dim,
+    Animations.FadeIn,
+  ]
 })
 export class ActionComponent{
   @Input() data: Action;
-  constructor() { }
+  state: string = 'inactive';
+  
+  mouseover() {
+    if (this.state === 'inactive') {
+      this.state = 'hover';
+    }
+  }
 
+  mouseleave() {
+    if (this.state === 'hover') {
+      this.state = 'inactive';
+    }
+  }
+
+  edit() {
+    if (this.state === 'hover') {
+      this.state = 'edit';
+    }
+  }
+
+  save() {
+    if (this.state === 'edit') {
+      this.state = 'inactive';
+    }
+  }
 }
